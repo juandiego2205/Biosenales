bde746cedd34ed8ea92a487687974de7e8dc423b
uandiego2205 <juand.caguasango.edu.co>
2024-08-16 07:29:39 -0500

    cambios

---
+++ Practica_1/Class1.py
@@ -0,0 +1,12 @@
+print("hola mundo")
+
+#git add .
+#git commmit -m
+#git push
+#git pull
+#git clone
+# .git ignore ./datos -- esa el la carpeta la cual NO se quiere subir al repositorio
+# cd.. /..
+#cd ./GIT/biosen-course
+
+print("hola buenas señora")
