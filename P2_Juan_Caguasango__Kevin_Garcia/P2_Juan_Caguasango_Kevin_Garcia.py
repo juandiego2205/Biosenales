f2fc44d2aff8cd7a0aaea6ab4095a0c195eb1748
uandiego2205 <juand.caguasango.edu.co>
2024-08-30 07:06:04 -0500

    update p2

--- P2_Juan_Caguasango__Kevin_Garcia/P2_Juan_Caguasango_Kevin_Garcia.py
+++
@@ -1,8 +0,0 @@
-import matplotlib as plt
-def impseq(n0, n1, n2):
-# Genera x(n) = delta(n-n0); n1 <= n <= n2
-# ----------------------------------------------
- n = np.arange(n1,n2+1) # Se crea el vector de muestras
- x = (n-n0) == 0
- return [x,n]
-plt.stem()
